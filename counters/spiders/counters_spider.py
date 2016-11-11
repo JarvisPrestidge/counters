8a8b8d7723487b103f6d40a91dcb95cf7b58a55b
jprestid <jprestid@JPRESTID-GB.uk.oracle.com>
2016-11-11 16:34:40 +0000

    Intermediatary commit, while moving workspaces

--- counters/spiders/counters_spider.py
+++ counters/spiders/counters_spider.py
@@ -1,17 +1,46 @@
 import scrapy
+import requests
+
 
 class CountersSpider(scrapy.Spider):
+
+    base_url = "http://champion.gg/champion/"
+    start_urls = []
+
+    def __init__(self):
+        # query riot static data api for champions list
+        r = requests.get("https://global.api.pvp.net/api/lol/static-data/"
+                         "euw/v1.2/champion?api_key=81962b95-cdc4-42de-bb2"
+                         "8-9c8a6736aeaa")
+        r.encoding = 'UTF-8'
+        data = r.json()
+        # list comprehension for champions
+        champions = [data['data'].values()[x]['name']
+                     for x in range(len(data['data']))]
+        # create individual champ urls
+        start_urls = [("%s:%s" % )for x in range(champions)]
+
     name = "counters"
+    allowed_domains = ['champion.gg']
 
     start_urls = [
-            'http://champion.gg/champion/riven/'
+        'http://champion.gg/champion/riven/'
     ]
 
     def parse(self, response):
-        champion = 
-        role =
-        data =
-        patch =
-        filename = 'output.json'
-        with open(filename, 'wb') as f:
-            f.write(stats)
+        # get champion data
+        champion = response.xpath(
+            "/html/body/div[1]/div/div[3]/script[1]/text()").re(
+            r"(?:matchupData\.champion.?=.?)(\{[^;]*)")[0]
+        # get general role data
+        role = response.xpath(
+            "/html/body/div[1]/div/div[3]/script[1]/text()").re(
+            r"(?:matchupData\.generalRole.?=.?)(\{[^;]*)")[0]
+        # get big data
+        data = response.xpath(
+            "/html/body/div[1]/div/div[3]/script[1]/text()").re(
+            r"(?:matchupData\.championData.?=.?)(\{[^;]*)")[0]
+        # get patch data
+        patch = response.xpath(
+            "/html/body/div[1]/div/div[3]/script[1]/text()").re(
+            r"(?:matchupData\.patchHistory.?=.?)(\[[^;]*)")[0]
