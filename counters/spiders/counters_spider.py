da63bb82f4255445e186489b885db4e9551d8b38
Jarvis Prestidge <Jarvis Prestidge>
2016-11-12 00:10:50 +0000

    Populate the start_urls list with all champion names from riot api
    The string "Wukong" from the riot static data champion api doesn't align with champion.gg's page for wukong. The actual page url has "MonkeyKing" hence the need to search and replace that element with the correct substitution.

--- counters/spiders/counters_spider.py
+++ counters/spiders/counters_spider.py
@@ -1,31 +1,30 @@
 import scrapy
 import requests
+import re
 
 
 class CountersSpider(scrapy.Spider):
 
-    base_url = "http://champion.gg/champion/"
-    start_urls = []
-
     def __init__(self):
+        base_url = "http://champion.gg/champion/"
         # query riot static data api for champions list
         r = requests.get("https://global.api.pvp.net/api/lol/static-data/"
                          "euw/v1.2/champion?api_key=81962b95-cdc4-42de-bb2"
                          "8-9c8a6736aeaa")
         r.encoding = 'UTF-8'
         data = r.json()
-        # list comprehension for champions
-        champions = [data['data'].values()[x]['name']
-                     for x in range(len(data['data']))]
+        # list of champion names (with Wukong as MonkeyKing)
+        champs = ["MonkeyKing" if data['data'].values()[x]['name'] == "Wukong"
+                  else data['data'].values()[x]['name']
+                  for x in range(len(data['data']))]
+        # regex to remove all non-alpha
+        non_alpha = re.compile('[^a-zA-Z]')
         # create individual champ urls
-        start_urls = [("%s:%s" % )for x in range(champions)]
+        self.start_urls = [
+            ("%s%s" % (base_url, non_alpha.sub("", x))) for x in champs]
 
     name = "counters"
     allowed_domains = ['champion.gg']
-
-    start_urls = [
-        'http://champion.gg/champion/riven/'
-    ]
 
     def parse(self, response):
         # get champion data
