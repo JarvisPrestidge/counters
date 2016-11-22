1e7a63f5776107a23d6369e387e0783439f8f2a6
Jarvis Prestidge <jarvisprestidge@gmail.com>
2016-11-22 00:26:34 +0000

    Began adding of item fields

--- counters/items.py
+++ counters/items.py
@@ -8,7 +8,45 @@
 import scrapy
 
 
-class CountersItem(scrapy.Item):
+class ChampionItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    win_rate = scrapy.Field()
+    play_rate = scrapy.Field()
+    ban_rate = scrapy.Field()
+    avg_played = scrapy.Field()
+    gold_earned = scrapy.Field()
+    kills = scrapy.Field()
+    deaths = scrapy.Field()
+    assists = scrapy.Field()
+    dmg_dealt = scrapy.Field()
+    dmg_taken = scrapy.Field()
+    minions_killed = scrapy.Field()
+
+
+class RoleItem(scrapy.Item):
+
+    # first highest percentage role
+    role_1 = scrapy.Field()
+    percent_played_1 = scrapy.Field()
+    games_played_1 = scrapy.Field()
+
+    # second highest percentage role
+    role_2 = scrapy.Field()
+    percent_played_2 = scrapy.Field()
+    games_played_2 = scrapy.Field()
+
+    # jungle role
+    role_jungle = scrapy.Field()
+    percent_played_jungle = scrapy.Field()
+    games_played_jungle = scrapy.Field(
+
+
+class DataItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+class PatchItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
